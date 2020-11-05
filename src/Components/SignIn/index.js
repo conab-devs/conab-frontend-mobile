290670a351300cd36ff8e967eb741b358b6ae2f5
Hermerson Araújo <ne.hermerson@gmail.com>
2020-11-05 09:35:17 -0300

    style: add SignIn and SingOut components

---
+++ src/Components/SignIn/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const SignIn = () => (
+  <View>
+    <Text>Sign In</Text>
+  </View>
+);
+
+export default SignIn;
---
+++ src/Components/SignUp/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const SignOut = () => (
+  <View>
+    <Text>Sign Out</Text>
+  </View>
+);
+
+export default SignOut;
