899000792c6fbc8b39948d1f434e5cdf44a7972d
Hermerson Araújo <ne.hermerson@gmail.com>
2020-11-20 11:28:59 -0300

    feat: start image upload

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -1,9 +1,19 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import ImagePicker from 'react-native-image-picker';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import {Container, Form} from './styles';
+import {Container, Form, Upload, UploadImage} from './styles';
+import {darkblue} from '../../styles/colors';
+import {PermissionsAndroid, Platform} from 'react-native';
+
+const launchImageLibraryAsync = (options = {}) => {
+  return new Promise((resolve) =>
+    ImagePicker.launchImageLibrary(options, resolve),
+  );
+};
 
 const SignUp = () => {
   const [name, setName] = useState('');
@@ -11,12 +21,47 @@ const SignUp = () => {
   const [phone, setPhone] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [uri, setUri] = useState(null);
+  const [canUploadOnAndroid, setCanUploadOnAndroid] = useState(false);
+
+  useEffect(() => {
+    (async function requestAndroidPermission() {
+      const permission = PermissionsAndroid.PERMISSIONS.CAMERA;
+      const hasPermission = await PermissionsAndroid.check(permission);
+
+      if (!hasPermission) {
+        const status = await PermissionsAndroid.request(permission);
+        setCanUploadOnAndroid(
+          Platform.OS === 'android' && status === 'granted',
+        );
+      }
+
+      setCanUploadOnAndroid(hasPermission);
+    })();
+  }, []);
 
   const submitHandler = () => {};
+
+  const uplaodImage = async () => {
+    try {
+      const result = await launchImageLibraryAsync();
+      console.log(result.uri);
+      setUri(result.uri);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <Container>
       <Form>
+        <Upload onPress={uplaodImage}>
+          {uri ? (
+            <UploadImage source={{uri}} />
+          ) : (
+            <Icon name="camera" size={40} color={darkblue} />
+          )}
+        </Upload>
         <Input
           label="Nome Completo"
           value={name}
--- src/screens/SignUp/styles.js
+++ src/screens/SignUp/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import {lightgray} from '../../styles/colors';
 
 import {Container as C} from '../../styles/utils';
 
@@ -7,7 +8,27 @@ export const Container = styled(C)`
   justify-content: center;
 `;
 
-export const Form = styled.View`
+export const Form = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: {paddingTop: 20, paddingBottom: 20},
+})`
   margin-top: 20px;
   width: 100%;
+`;
+
+export const Upload = styled.TouchableOpacity`
+  align-self: center;
+  height: 100px;
+  width: 100px;
+  border-radius: 5px;
+  justify-content: center;
+  align-items: center;
+  background-color: ${lightgray};
+  margin-bottom: 20px;
+  overflow: hidden;
+`;
+
+export const UploadImage = styled.Image`
+  width: 100%;
+  height: 100%;
 `;
