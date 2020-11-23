7bf0dee2c95323fb0c850c8e831622ccb7205a32
Hermerson Araújo <ne.hermerson@gmail.com>
2020-11-23 09:53:52 -0300

    refactor(style): button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {Btn, BtnText} from './styles';
 
 const Button = ({type, title, ...buttonProps}) => (
-  <Btn {...buttonProps}>
+  <Btn type={type} {...buttonProps}>
     <BtnText type={type}>{title}</BtnText>
   </Btn>
 );
--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -7,29 +7,22 @@ export const Btn = styled.TouchableOpacity`
   width: 100%;
   align-items: center;
   justify-content: center;
-  background-color: ${darkblue};
   height: 46px;
-  padding: 2px;
-`;
-
-export const BtnText = styled.Text`
-  color: white;
-  height: 100%;
-  width: 100%;
-  text-align: center;
-  line-height: 44px;
-  border-radius: 3px;
-  font-size: 24px;
 
   ${({type}) => {
     return type === 'primary'
       ? css`
-          background-color: transparent;
-          color: white;
+          background-color: ${darkblue};
         `
       : css`
           background-color: ${green};
-          color: ${darkblue};
+          border-width: 2px;
         `;
   }}
+`;
+
+export const BtnText = styled.Text`
+  color: white;
+  font-size: 24px;
+  color: ${({type}) => (type === 'primary' ? 'white' : darkblue)};
 `;
