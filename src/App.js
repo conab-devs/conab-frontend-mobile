6cf4628b33f48ac600af32923b3f10757209b2cf
Franklyn Medeiros <franklynkleiton18@gmail.com>
2020-12-19 17:06:24 -0300

    Remove context

--- src/App.js
+++ src/App.js
@@ -11,27 +11,17 @@ import './reactotron';
 import {darkblue} from './styles/colors';
 import {store, persistor} from './redux';
 import {Provider as PaperProvider} from 'react-native-paper';
-import {FilterContext} from './contexts';
 import {maxPrice} from './variables';
 
 const App = () => {
-  const [settings, setSettings] = useState({
-    lowestPrice: 0,
-    greatestPrice: maxPrice,
-    order: 'asc',
-    searchString: '',
-  });
-
   return (
     <Provider store={store}>
       <PaperProvider>
         <PersistGate persistor={persistor}>
-          <FilterContext.Provider value={{settings, setSettings}}>
-            <NavigationContainer>
-              <StatusBar barStyle="light-content" backgroundColor={darkblue} />
-              <Routes />
-            </NavigationContainer>
-          </FilterContext.Provider>
+          <NavigationContainer>
+            <StatusBar barStyle="light-content" backgroundColor={darkblue} />
+            <Routes />
+          </NavigationContainer>
         </PersistGate>
       </PaperProvider>
     </Provider>
