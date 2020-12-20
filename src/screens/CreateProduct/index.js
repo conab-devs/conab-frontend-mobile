f75f056a5267302ac704c607c697d12762f6e88b
Franklyn Medeiros <franklynkleiton18@gmail.com>
2020-12-20 16:28:04 -0300

    refactor: Creating product form component

--- src/screens/CreateProduct/index.js
+++ src/screens/CreateProduct/index.js
@@ -1,57 +1,38 @@
-import React, {useState, useCallback} from 'react';
-import {StyleSheet, ScrollView} from 'react-native';
-import {
-  Container,
-  Title,
-  Label,
-  Group,
-  DropdownHolder,
-  DeliveryTime,
-  DeliveryTimeInput,
-  MeasureContainer,
-  Measure,
-  Input,
-  Button,
-  ButtonContent,
-  ImageHolder,
-  Image,
-} from './styles';
-import Dropdown from 'react-native-picker-select';
-import {TextInputMask} from 'react-native-masked-text';
+import React, {useCallback} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import ImagePicker from 'react-native-image-crop-picker';
 import {createFormData} from '../../helpers';
 import {allActions} from '../../redux/Product';
+import Form from '../../components/ProductForm';
 
 const CreateProduct = ({navigation}) => {
-  const [name, setName] = useState('');
-  const [category, setCategory] = useState(0);
-  const [unitOfMeasure, setUnitOfMeasure] = useState('');
-  const [price, setPrice] = useState('0');
-  const [deliveryTime, setDeliveryTime] = useState('');
-  const [productPicture, setProductPicture] = useState('');
-
   const {categories} = useSelector((state) => state.product);
   const dispatch = useDispatch();
 
-  let money = null;
+  const handleSubmit = useCallback(
+    (requestFields) => {
+      if (
+        requestFields.price > 0 &&
+        requestFields.category > 0 &&
+        typeof requestFields.deliveryTime === 'number' &&
+        requestFields.name !== '' &&
+        (requestFields.unitOfMeasure === 'kg' ||
+          requestFields.unitOfMeasure === 'unit') &&
+        requestFields.productPicture.path !== ''
+      ) {
+        const data = createFormData(requestFields.productPicture, {
+          name: requestFields.name,
+          price: requestFields.price,
+          category_id: requestFields.category,
+          estimated_delivery_time: requestFields.deliveryTime,
+          unit_of_measure: requestFields.unitOfMeasure,
+        });
 
-  const handleImagePicking = useCallback(() => {
-    const image = ImagePicker.openPicker({
-      width: 300,
-      height: 300,
-      cropping: true,
-    })
-      .then((image) => {
-        if (image.path) {
-          setProductPicture(image);
-        }
-      })
-      .catch((err) => {
-        return;
-      });
-  }, []);
+        dispatch(allActions.createProduct({product: data}));
+        navigation.goBack();
+      }
+    },
+    [navigation, dispatch],
+  );
 
   const getCategories = useCallback(() => {
     return categories.map((category) => ({
@@ -61,149 +42,13 @@ const CreateProduct = ({navigation}) => {
   }, [categories]);
 
   return (
-    <ScrollView>
-      <Container>
-        <Title>Adicionar Produto</Title>
-
-        <ImageHolder>
-          {productPicture ? (
-            <Image source={{uri: productPicture.path}} />
-          ) : null}
-          <Icon
-            name="image-edit"
-            size={20}
-            color="#000000"
-            style={{
-              position: 'absolute',
-              right: 15,
-              bottom: 15,
-            }}
-            onPress={handleImagePicking}
-          />
-        </ImageHolder>
-
-        <Group>
-          <Label>Nome</Label>
-
-          <Input
-            placeholder="Insira o nome do seu produto..."
-            onChangeText={setName}
-          />
-        </Group>
-
-        <Group>
-          <Label>Categoria</Label>
-
-          <DropdownHolder>
-            <Dropdown
-              placeholder={{
-                label: 'Escolha a categoria do produto',
-                value: null,
-              }}
-              onValueChange={setCategory}
-              style={stylesDropdown}
-              items={getCategories()}
-            />
-          </DropdownHolder>
-        </Group>
-
-        <Group>
-          <Label>Unidade de Medida</Label>
-
-          <DropdownHolder>
-            <Dropdown
-              placeholder={{
-                label: 'Escolha uma unidade de medida',
-                value: null,
-              }}
-              onValueChange={setUnitOfMeasure}
-              style={stylesDropdown}
-              items={[
-                {label: 'Kg', value: 'kg'},
-                {label: 'Unidade', value: 'unit'},
-              ]}
-            />
-          </DropdownHolder>
-        </Group>
-
-        <Group>
-          <Label>Preço</Label>
-
-          <TextInputMask
-            type={'money'}
-            value={price}
-            style={styles.masked}
-            onChangeText={setPrice}
-            ref={(ref) => (money = ref)}
-          />
-        </Group>
-
-        <Group>
-          <Label>Tempo de Entrega</Label>
-
-          <DeliveryTime>
-            <DeliveryTimeInput
-              keyboardType="numeric"
-              defaultValue="0"
-              value={`${deliveryTime}`}
-              onChangeText={(value) => setDeliveryTime(value * 1)}
-              placeholder="Insira o tempo de Entrega..."
-            />
-            <MeasureContainer>
-              <Measure>Dias</Measure>
-            </MeasureContainer>
-          </DeliveryTime>
-        </Group>
-
-        <Button>
-          <ButtonContent
-            onPress={() => {
-              const rawPrice = money.getRawValue();
-
-              if (
-                rawPrice > 0 &&
-                category > 0 &&
-                typeof deliveryTime === 'number' &&
-                name !== '' &&
-                (unitOfMeasure === 'kg' || unitOfMeasure === 'unit') &&
-                productPicture.path !== ''
-              ) {
-                const data = createFormData(productPicture, {
-                  name,
-                  price: rawPrice,
-                  category_id: category,
-                  estimated_delivery_time: deliveryTime,
-                  unit_of_measure: unitOfMeasure,
-                });
-
-                dispatch(allActions.createProduct({product: data}));
-                navigation.goBack();
-              }
-            }}>
-            Adicionar
-          </ButtonContent>
-        </Button>
-      </Container>
-    </ScrollView>
+    <Form
+      title="Adicionar Produtos"
+      categories={getCategories()}
+      handleSubmit={handleSubmit}
+      buttonDescriptor="Adicionar"
+    />
   );
 };
-
-const stylesDropdown = StyleSheet.create({
-  inputAndroid: {
-    backgroundColor: '#F5F5F5',
-    color: '#828282',
-  },
-  inputIOS: {
-    backgroundColor: '#F5F5F5',
-    color: '#828282',
-  },
-});
-
-const styles = StyleSheet.create({
-  masked: {
-    backgroundColor: '#F5F5F5',
-    borderRadius: 5,
-  },
-});
 
 export default CreateProduct;
--- src/screens/Products/index.js
+++ src/screens/Products/index.js
@@ -10,7 +10,7 @@ import {
   Wrapper,
   FlatList,
   AddProductButton,
-  AddProductContent
+  AddProductContent,
 } from './styles';
 import Product from '../../components/Product';
 import {useSelector, useDispatch} from 'react-redux';
